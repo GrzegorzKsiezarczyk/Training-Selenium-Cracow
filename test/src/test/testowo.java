7c0198d4dacfafa3b433e54fad2be00e3291c622
grzegorzksiezarczyk <grzegorzksiezarczyk@192.168.0.16>
2018-05-29 20:25:54 +0200

    configurate selenium Web`driver

---
+++ test/src/test/testowo.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class testowo {
+	public static void main(String[] args) {
+		WebDriver driver =new SafariDriver();
+		driver.get("http://www.google.com");
+	}
+}
