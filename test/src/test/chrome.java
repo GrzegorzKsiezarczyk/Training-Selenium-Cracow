5527106d86b3af1f2efae8b7d842fcd4b5a27502
grzegorzksiezarczyk <grzegorzksiezarczyk@mbp-grzegorz>
2018-06-01 18:53:38 +0200

    Find elements by id,css selector. I use senkeys, submit, visiblityofElementlocated, click, on chrome webdriver.

--- test/src/test/chrome.java
+++ test/src/test/chrome.java
@@ -1,6 +1,11 @@
 package test;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class chrome {
@@ -8,9 +13,32 @@ public class chrome {
 		
 		
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/bin/chromedriver"); 
-		WebDriver driver1=new ChromeDriver();               
-        driver1.get("http://www.gmail.com");    
-                                                                        
+		WebDriver driver1=new ChromeDriver();
 		
-	}
-}
+		WebDriverWait wait = new WebDriverWait(driver1, 10);
+		//WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("")));
+		
+		
+        driver1.get("http://www.google.com");    
+        WebElement searchfield = driver1.findElement(By.id("lst-ib"));      //search element with Id                                                        
+		searchfield.sendKeys("facebook");                       //send text to google
+        searchfield.submit();     //accept 
+        
+        WebElement foto = driver1.findElement(By.linkText("Facebook – zaloguj się lub zarejestruj"));
+        foto.click();                   //click on facebook
+        
+       
+        //css selector
+
+        //Find first name text box by CSS Selector
+        driver1.navigate().to("https://www.linkedin.com/");
+        WebElement firstNameTextBox = driver1.findElement(By.cssSelector("#first-name"));
+        firstNameTextBox.click();
+        
+        
+        
+        
+        
+	}}
+        
+        
